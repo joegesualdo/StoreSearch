14ca9ed03f92b06df03fdeee8053254fe1fad8b0
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-10 10:34:50 -0400

    Get search bar working with fake results

--- StoreSearch/Classes/ViewControllers/SearchViewController.m
+++ StoreSearch/Classes/ViewControllers/SearchViewController.m
@@ -8,7 +8,8 @@
 
 #import "SearchViewController.h"
 
-@interface SearchViewController ()
+// hook up the data source and delegate protocols yourself.
+@interface SearchViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 
 // Why do we make the IBOutlets weak?
 // Recall that as soon as an object no longer has any strong pointers, it goes away (it is deallocated) and all the weak pointers become nil. Per Apple’s recommendation you’ve been making your outlets weak pointers. You may be wondering, if the pointers to these view objects are weak, then won’t the objects get deallocated too soon?
@@ -16,9 +17,14 @@
 @property (nonatomic, weak) IBOutlet UISearchBar *searchBar;
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
 
+
 @end
 
 @implementation SearchViewController
+{
+    // instance variables
+    NSMutableArray *_searchResults;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -42,6 +48,56 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView
+    numberOfRowsInSection:(NSInteger)section {
+  if (_searchResults == nil) {
+    return 0;
+  } else {
+    return [_searchResults count];
+  }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+  // You create a UITableViewCell by hand and put the data for this row into its text label.
+  static NSString *CellIdentifier = @"SearchResultCell";
+  UITableViewCell *cell =
+      [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  if (cell == nil) {
+    cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                  reuseIdentifier:CellIdentifier];
+  }
+  cell.textLabel.text = _searchResults[indexPath.row];
+  return cell;
+}
+
+#pragma mark - UISearchBarDelegate
+// is invoked when the user taps the Search button on the keyboard.
+
+// this delegate method will will put some fake data into this array and then use it to fill up the table.
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+  // tells the UISearchBar that it should no longer listen to keyboard input and as a result, the keyboard will hide itself until you tap inside the search bar again.
+   [searchBar resignFirstResponder];
+  //allocate a new NSMutableArray object and put it into the _searchResults instance variable. This is done each time the user performs a search.
+  _searchResults = [NSMutableArray arrayWithCapacity:10];
+  // You add a string with some text into the array. Just for the fun of it, that is repeated 3 times so your data model will have three rows in it.
+  for (int i = 0; i < 3; i++) {
+    [_searchResults
+        addObject:[NSString stringWithFormat:@"Fake Result %d for '%@'", i,
+                                             searchBar.text]];
+  // The last statement in the method reloads the table view to make the new rows visible, which means you have to adapt the table view data source methods to read from this array as well.
+  [self.tableView reloadData];
+  }
+}
+
+// The search bar has an ugly white gap above it. This removes it
+// This is part of the SearchBarDelegate protocol.
+- (UIBarPosition)positionForBar:(id<UIBarPositioning>)bar {
+  return UIBarPositionTopAttached;
 }
 
 @end
