0b0b493346a9d8505b93a4e84d89fe3c944afda9
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-12 11:30:20 -0400

    Create a landscape nib and controller; don't hook it up yet

---
+++ StoreSearch/Classes/ViewControllers/LandscapeViewController.h
@@ -0,0 +1,13 @@
+//
+//  LandscapeViewController.h
+//  StoreSearch
+//
+//  Created by Joe Gesualdo on 9/12/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LandscapeViewController : UIViewController
+
+@end
---
+++ StoreSearch/Classes/ViewControllers/LandscapeViewController.m
@@ -0,0 +1,38 @@
+//
+//  LandscapeViewController.m
+//  StoreSearch
+//
+//  Created by Joe Gesualdo on 9/12/14.
+//  Copyright (c) 2014 Joe Gesualdo. All rights reserved.
+//
+
+#import "LandscapeViewController.h"
+
+@interface LandscapeViewController ()
+
+@end
+
+@implementation LandscapeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
