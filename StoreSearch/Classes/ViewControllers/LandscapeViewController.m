37e249b95a16f244a50c59302444ab75fa998a03
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-12 12:58:57 -0400

    Add a scroll view to the landscape view

--- StoreSearch/Classes/ViewControllers/LandscapeViewController.m
+++ StoreSearch/Classes/ViewControllers/LandscapeViewController.m
@@ -10,6 +10,9 @@
 
 @interface LandscapeViewController ()
 
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, weak) IBOutlet UIPageControl *pageControl;
+
 @end
 
 @implementation LandscapeViewController
@@ -27,6 +30,13 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    // first you put an image on the scroll view’s background so you can actually see something happening when you scroll through it.
+    // An image? But you’re setting the backgroundColor property, which is a UIColor, not a UIImage?! Yup, that’s true, but UIColor has a cool trick that lets you use a tile- able image for a color. If you take a peek at the LandscapeBackground.png image in the asset catalog you’ll see that it is a small square. By setting this image as a pattern image on the background, you get a repeatable image that fills the whole screen. You can use tile-able images anywhere you can use a UIColor.
+    
+    self.scrollView.backgroundColor = [UIColor colorWithPatternImage: [UIImage imageNamed:@"LandscapeBackground"]];
+    // The second thing, and this is very important when dealing with scroll views, is that you set the contentSize property. This will tell the scroll view how big its insides are. You don’t change the frame (or bounds) of the scroll view if you want its insides to be bigger, you set the contentSize property instead. People often forget this step and then they wonder why their scroll view doesn’t scroll. Unfortunately, you cannot do this from Interface Builder, so it must be done from within the code.
+    self.scrollView.contentSize = CGSizeMake(1000, self.scrollView.bounds.size.height);
 }
 
 - (void)didReceiveMemoryWarning
